436f4b805a0db8fd160bb073d8499e45306b97f5
arjrao <arjun.rao1@blackrock.com>
2016-12-16 17:54:06 -0500

    Committing test class for CodeCov

--- test/org/usfirst/frc/team4571/robot/TestClass.java
+++ test/org/usfirst/frc/team4571/robot/TestClass.java
@@ -1,5 +1,12 @@
 package org.usfirst.frc.team4571.robot;
 
-public class TestClass {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class TestClass {
+	
+	@Test
+	public void testIfTrue(){
+		Assert.assertFalse(5<4);
+	}
 }
